62d411f8845741a9f853cf7a47620c7e55500c99
Beshar-Elhatib <besharelhatib@gmail.com>
2024-06-10 08:57:55 +0300

    Add project files.

---
+++ 02_pPictureProje/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace _02_pPictureProje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbBoy = new System.Windows.Forms.RadioButton();
+            this.rbGirl = new System.Windows.Forms.RadioButton();
+            this.rbBook = new System.Windows.Forms.RadioButton();
+            this.rbPen = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbImage
+            // 
+            this.pbImage.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pbImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pbImage.ErrorImage = null;
+            this.pbImage.Image = global::_02_pPictureProje.Properties.Resources.Boy;
+            this.pbImage.InitialImage = null;
+            this.pbImage.Location = new System.Drawing.Point(153, 73);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(476, 315);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbImage.TabIndex = 0;
+            this.pbImage.TabStop = false;
+            this.pbImage.Click += new System.EventHandler(this.pbImage_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(349, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // rbBoy
+            // 
+            this.rbBoy.AutoSize = true;
+            this.rbBoy.Location = new System.Drawing.Point(182, 394);
+            this.rbBoy.Name = "rbBoy";
+            this.rbBoy.Size = new System.Drawing.Size(53, 21);
+            this.rbBoy.TabIndex = 2;
+            this.rbBoy.TabStop = true;
+            this.rbBoy.Text = "Boy";
+            this.rbBoy.UseVisualStyleBackColor = true;
+            this.rbBoy.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rbGirl
+            // 
+            this.rbGirl.AutoSize = true;
+            this.rbGirl.Location = new System.Drawing.Point(306, 394);
+            this.rbGirl.Name = "rbGirl";
+            this.rbGirl.Size = new System.Drawing.Size(47, 21);
+            this.rbGirl.TabIndex = 3;
+            this.rbGirl.TabStop = true;
+            this.rbGirl.Text = "Girl";
+            this.rbGirl.UseVisualStyleBackColor = true;
+            this.rbGirl.CheckedChanged += new System.EventHandler(this.rbGirl_CheckedChanged);
+            // 
+            // rbBook
+            // 
+            this.rbBook.AutoSize = true;
+            this.rbBook.Location = new System.Drawing.Point(434, 394);
+            this.rbBook.Name = "rbBook";
+            this.rbBook.Size = new System.Drawing.Size(60, 21);
+            this.rbBook.TabIndex = 4;
+            this.rbBook.TabStop = true;
+            this.rbBook.Text = "Book";
+            this.rbBook.UseVisualStyleBackColor = true;
+            this.rbBook.CheckedChanged += new System.EventHandler(this.rbBook_CheckedChanged);
+            // 
+            // rbPen
+            // 
+            this.rbPen.AutoSize = true;
+            this.rbPen.Location = new System.Drawing.Point(549, 394);
+            this.rbPen.Name = "rbPen";
+            this.rbPen.Size = new System.Drawing.Size(52, 21);
+            this.rbPen.TabIndex = 5;
+            this.rbPen.TabStop = true;
+            this.rbPen.Text = "Pen";
+            this.rbPen.UseVisualStyleBackColor = true;
+            this.rbPen.CheckedChanged += new System.EventHandler(this.rbPen_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rbPen);
+            this.Controls.Add(this.rbBook);
+            this.Controls.Add(this.rbGirl);
+            this.Controls.Add(this.rbBoy);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbBoy;
+        private System.Windows.Forms.RadioButton rbGirl;
+        private System.Windows.Forms.RadioButton rbBook;
+        private System.Windows.Forms.RadioButton rbPen;
+    }
+}
+
---
+++ 02_pPictureProje/Form1.cs
@@ -0,0 +1,56 @@
+﻿using _02_pPictureProje.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02_pPictureProje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            pbImage.Image = Resources.Boy;
+        //    pbImage.Image = Properties.Resources.Boy;
+            label1.Text = "Boy";
+          //  pbImage.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void rbBook_CheckedChanged(object sender, EventArgs e)
+        {
+            pbImage.Image =Resources.Book;
+            label1.Text = "Book";
+        }
+
+        private void rbGirl_CheckedChanged(object sender, EventArgs e)
+        {
+            pbImage.Image = Resources.Girl;
+            label1.Text = "Girl";
+            //pbImage.SizeMode = PictureBoxSizeMode.Zoom;
+
+
+        }
+
+        private void rbPen_CheckedChanged(object sender, EventArgs e)
+        {
+            pbImage.Image = Resources.Pen;
+            label1.Text = "Pen";
+          //  pbImage.SizeMode = PictureBoxSizeMode.Zoom; 
+        }
+
+        private void pbImage_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
